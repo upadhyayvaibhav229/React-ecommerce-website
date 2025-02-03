09736de370073ec2d5b874bdac25fffcf335beb6
upadhyayvaibhav229 <upadhyayvaibhav229@gmail.com>
2025-02-03 16:01:32 +0530

    commited

--- ecommerce-site/src/Header.jsx
+++ ecommerce-site/src/Header.jsx
@@ -1,16 +1,27 @@
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { getCartCount } from "./Features/cartSlice";  // Importing the selector
+import { getCartCount } from "./Features/cartSlice";
 import { setSearchTerm } from "./Features/shopSlice";
 import { Search, ShoppingCart } from "lucide-react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+
 const Header = () => {
   const [showSearch, setShowSearch] = useState(false);
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const dispatch = useDispatch();
   const { searchTerm } = useSelector(state => state.shop);
-  const totalQuantity = useSelector(getCartCount);  // Using the selector here
+  const totalQuantity = useSelector(getCartCount);
+  const location = useLocation();
+
   const handleSearch = (e) => dispatch(setSearchTerm(e.target.value));
+
+  const navLinks = [
+    { name: "Home", path: "/" },
+    { name: "Collection", path: "/collection" },
+    { name: "About", path: "/about" },
+    { name: "Contact", path: "/contact" },
+    // { name: "", path: "/contact" },
+  ];
 
   return (
     <header className="shadow-md bg-white">
@@ -20,6 +31,25 @@ const Header = () => {
           <img src="/path/to/logo.png" className="h-12 cursor-pointer" alt="Logo" />
         </Link>
 
+        {/* Navbar Links */}
+        <ul className="flex gap-6 text-gray-700 font-medium">
+          {navLinks.map((link) => (
+            <li key={link.name}>
+              <Link
+                to={link.path}
+                className={`relative pb-2 ${
+                  location.pathname === link.path ? "text-blue-600" : ""
+                }`}
+              >
+                {link.name}
+                {location.pathname === link.path && (
+                  <span className="absolute left-0 bottom-0 w-full h-0.5 bg-blue-600"></span>
+                )}
+              </Link>
+            </li>
+          ))}
+        </ul>
+
         {/* Icons */}
         <div className="flex items-center gap-5">
           <button onClick={() => setShowSearch(!showSearch)}>
@@ -27,10 +57,7 @@ const Header = () => {
           </button>
 
           <Link to="/cart" className="relative">
-            {/* Shopping Cart Icon */}
             <ShoppingCart className="w-5 h-5 cursor-pointer text-gray-700" />
-
-            {/* Display the cart quantity */}
             {totalQuantity > 0 && (
               <span className="absolute top-0 right-0 flex items-center justify-center w-4 h-4 text-white bg-red-500 rounded-full text-xs">
                 {totalQuantity}
--- ecommerce-site/src/Pages/Cart.jsx
+++ ecommerce-site/src/Pages/Cart.jsx
@@ -1,40 +1,33 @@
-import React, { useEffect, useState } from 'react'
-import { useSelector } from 'react-redux'
-import { selectShop } from '../Features/shopSlice'
-import Header from '../Header'
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { selectShop } from "../Features/shopSlice";
+import Header from "../Header";
+import Title from "../Components/Title";
 
 const Cart = () => {
-    const cart = useSelector((state) => state.cart)
-    const {products, currency} = useSelector(selectShop)
+  const cart = useSelector((state) => state.cart);
+  const { products, currency } = useSelector(selectShop);
 
-    const [cartData, setCartData] = useState([]);
+  const [cartData, setCartData] = useState([]);
 
-    useEffect(()=>{
-        const tempdata = []
-        for(const items in cart.cart){
-            for (const item in cart.cart[items]) {
-                if (cart.cart[items][item].id === item) {
-                    tempdata.push({
-                    _id: items,
-                    title:items,
-                    })
-                }
-            }
-            
-        }
-           
-        console.log(tempdata);
-    }, [cart.cart, products])
+console.log(cart);
 
 
   return (
     <>
-    
-    <div>
-      <p>Cart</p>
-    </div>
+      <div>
+       <Title text1={"Your"} text2={"Cart"}/>
+        {
+          cart.cart?.map((el,id)=>(
+            <div key={id}>
+              <p>{el.title}</p>
+              <p>{el.price}</p>
+            </div>
+          ))
+        }
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Cart
+export default Cart;
