dbca16ce6d6a85202918d36b6eb3da8516c1ddb9
upadhyayvaibhav229 <upadhyayvaibhav229@gmail.com>
2025-02-13 16:59:57 +0530

    commited

--- ecommerce-site/src/Config/firebaseConfig.js
+++ ecommerce-site/src/Config/firebaseConfig.js
@@ -1,18 +1,18 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCEYgGkauUCCzS7kU-_K_29YbRc8PuAfzc",
-  authDomain: "ecommerce-site-e7dc4.firebaseapp.com",
-  projectId: "ecommerce-site-e7dc4",
-  storageBucket: "ecommerce-site-e7dc4.appspot.com",
-  messagingSenderId: "462907253217",
-  appId: "1:462907253217:web:1efd0bf7643dbdd000b2a0",
-  measurementId: "G-WYCR650RK1"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
@@ -29,6 +29,5 @@ if (typeof window !== "undefined") {
   analytics = getAnalytics(app);
 }
 
-export { auth, analytics };
-
+export { auth, analytics, db };
 export default app;
