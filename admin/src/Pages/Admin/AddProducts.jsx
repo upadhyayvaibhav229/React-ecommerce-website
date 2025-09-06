48a6e4850030e9b631ffb428bb6609a19f9c7c9e
upadhyayvaibhav229 <upadhyayvaibhav229@gmail.com>
2025-09-06 23:24:31 +0530

    commteid

--- admin/src/Pages/Admin/AddProducts.jsx
+++ admin/src/Pages/Admin/AddProducts.jsx
@@ -30,17 +30,22 @@ const AddProducts = () => {
         </div>
         <div className="w-full">
           <p className='mb-2'>Product name</p>
-          <input className='w-full max-w-[500px] px-3 py-2 border border-gray-300 focus:border-[#c586a5]' type="text" placeholder='Type here' required />
+          <input
+            className="w-full max-w-[500px] px-3 py-2 border border-gray-300 rounded-md outline-none focus:border-[#c586a5]"
+            type="text"
+            placeholder="Type here"
+            required
+          />
         </div>
         <div className="w-full">
           <p className='mb-2'>Product name</p>
-          <textarea className='w-full max-w-[500px] px-3 py-2 border border-gray-300 focus:border-[#c586a5]' type="text" placeholder='Write Content here' required />
+          <textarea className='w-full max-w-[500px] px-3 py-2 border-2 border-gray-300 rounded-md outline-none focus:border-[#c586a5]' placeholder='Write Content here' required />
         </div>
 
         <div className="flex flex-col sm:flex-row gap-2 w-full sm:gap-8">
           <div>
             <p className='mb-2'>Product Category</p>
-            <select className='w-full px-3 py-2'>
+            <select className='w-full px-3 py-2 border-2 border-gray-300 rounded-md outline-none focus:border-[#c586a5]'>
               <option value="Men">Men</option>
               <option value="Women">Women</option>
               <option value="Kids">Kids</option>
@@ -48,8 +53,8 @@ const AddProducts = () => {
           </div>
 
           <div>
-            <p class="mb-2">Sub category</p>
-            <select class="w-full px-3 py-2">
+            <p className="mb-2">Sub category</p>
+            <select className="w-full px-3 py-2 border-2 border-gray-300 rounded-md outline-none focus:border-[#c586a5]">
               <option value="Topwear">Topwear</option>
               <option value="Bottomwear">Bottomwear</option>
               <option value="Winterwear">Winterwear</option>
@@ -58,9 +63,51 @@ const AddProducts = () => {
 
           <div>
             <p>Product Price</p>
-            <input class="w-full px-3 py-2 sm:w-[120px]" type="Number" placeholder="25" value=""></input>
+            <input class="w-full px-3 py-2 sm:w-[120px]  border-2 border-gray-300 rounded-md outline-none focus:border-[#c586a5]" type="Number" placeholder="25" value=""></input>
           </div>
         </div>
+
+        <div>
+          <p className='mb-2'>Product Sizes</p>
+          <div className="flex gap-3">
+            <div>
+              <p className="bg-slate-200 px-3 py-1 cursor-pointer ">
+                S
+              </p>
+            </div>
+
+            <div>
+              <p className="bg-slate-200 px-3 py-1 cursor-pointer ">
+                M
+              </p>
+            </div>
+            <div>
+              <p className="bg-slate-200 px-3 py-1 cursor-pointer ">
+                L
+              </p>
+            </div>
+            <div>
+              <p className="bg-slate-200 px-3 py-1 cursor-pointer ">
+                XL
+              </p>
+            </div>
+            <div>
+              <p className="bg-slate-200 px-3 py-1 cursor-pointer ">
+                XXL
+              </p>
+            </div>
+          </div>
+
+        </div>
+
+        <div className='flex gap-2 mt-2'>
+          <input type="checkbox"  id="bestseller" />
+          <label htmlFor="bestseller" className=' cursor-pointer'>
+            Add to bestseller
+          </label>
+        </div>
+
+        <button type="submit" className='w-28 py-3 mt-4 bg-black text-white hover:bg-white hover:text-black hover:border-2 hover:border-black rounded cursor-pointer'>ADD</button>
       </form>
     </>
   )
--- admin/src/Pages/Admin/ProductLists.jsx
+++ admin/src/Pages/Admin/ProductLists.jsx
@@ -2,9 +2,54 @@ import React from 'react'
 
 const ProductLists = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <p className='mb-2'>All Products</p>
+      <div className='flex flex-col gap-3 mb-2'>
+        <div className="hidden md:grid grid-cols-[1fr_3fr_1fr_1fr_1fr] bg-slate-200 px-2 py-3">
+          <b>Image</b>
+          <b>Name</b>
+          <b>Category</b>
+          <b>Price</b>
+          <b className='text-center'>Action</b>
+        </div>
+        <div className="grid grid-cols-[1fr_3fr_1fr] md:grid-cols-[1fr_3fr_1fr_1fr_1fr] items-center gap-2 py-1 px-2 border text-sm">
+          <img src="img" className='w-12' alt="img" />
+          <p>Kids Tappered Slim Fit Trousers</p>
+          <p>Kids</p>
+          <p>38₹</p>
+          <p className="text-right md:text-center cursor-pointer text-lg">X</p>
+        </div>
+        <div className="grid grid-cols-[1fr_3fr_1fr] md:grid-cols-[1fr_3fr_1fr_1fr_1fr] items-center gap-2 py-1 px-2 border text-sm">
+          <img src="img" className='w-12' alt="img" />
+          <p>Kids Tappered Slim Fit Trousers</p>
+          <p>Kids</p>
+          <p>38₹</p>
+          <p className="text-right md:text-center cursor-pointer text-lg">X</p>
+        </div>
+        <div className="grid grid-cols-[1fr_3fr_1fr] md:grid-cols-[1fr_3fr_1fr_1fr_1fr] items-center gap-2 py-1 px-2 border text-sm">
+          <img src="img" className='w-12' alt="img" />
+          <p>Kids Tappered Slim Fit Trousers</p>
+          <p>Kids</p>
+          <p>38₹</p>
+          <p className="text-right md:text-center cursor-pointer text-lg">X</p>
+        </div>
+        <div className="grid grid-cols-[1fr_3fr_1fr] md:grid-cols-[1fr_3fr_1fr_1fr_1fr] items-center gap-2 py-1 px-2 border text-sm">
+          <img src="img" className='w-12' alt="img" />
+          <p>Kids Tappered Slim Fit Trousers</p>
+          <p>Kids</p>
+          <p>38₹</p>
+          <p className="text-right md:text-center cursor-pointer text-lg">X</p>
+        </div>
+        <div className="grid grid-cols-[1fr_3fr_1fr] md:grid-cols-[1fr_3fr_1fr_1fr_1fr] items-center gap-2 py-1 px-2 border text-sm">
+          <img src="img" className='w-12' alt="img" />
+          <p>Kids Tappered Slim Fit Trousers</p>
+          <p>Kids</p>
+          <p>38₹</p>
+          <p className="text-right md:text-center cursor-pointer text-lg">X</p>
+        </div>
+
+      </div>
+    </>
   )
 }
 
